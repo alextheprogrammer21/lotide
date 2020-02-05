ef1608d852e58931925f038d122a5dde0b8eeba9
Alex <alextheprogrammer21@gmail.com>
2020-02-05 22:47:42 +0000

    e

--- countLetters.js
+++ countLetters.js
@@ -12,15 +12,16 @@ const assertEqual = function(actual, expected) {
 //const alphabets = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
 //Count Letters Main Function
 countLetters = (words) => {
+  const words2 = words.split(" ").join("");
   let count = {}; 
 
-  for (let i = 0; i < words.length; i++) {
-    if (count[words[i]]) { 
-      count[words[i]] += 1; 
-    } else { count[words[i]] = 1; }
+  for (let i = 0; i < words2.length; i++) {
+    if (count[words2[i]]) { 
+      count[words2[i]] += 1; 
+    } else { count[words2[i]] = 1; }
   }
   return count;
 }
 
-console.log(countLetters("Lighthouse"));
+console.log(countLetters("Lighthouse Labs"));
  
---
+++ letterPositions.js
@@ -0,0 +1,27 @@
+//ASSERT EQUALS VERIFICATION 
+const assertArraysEqual = function(arr1, arr2) {
+  if(JSON.stringify(arr1)===JSON.stringify(arr2)) {
+    console.log(`😎Assertion passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+}
+
+letterPositions = (sentence) => {
+  const sentence2 = sentence.split(" ").join("");
+
+  let result = {};
+
+  for (let i = 0; i < sentence2.length; i++) {
+    if (result[sentence2[i]]) {
+      result[sentence2[i]].push(i);
+    } else {
+      result[sentence2[i]] = [i]; 
+    }
+  }
+  return result;
+}
+
+console.log(letterPositions("lighthouse labs"));
+
+assertArraysEqual(letterPositions("lighthouse in the house")["l"], [0]);
