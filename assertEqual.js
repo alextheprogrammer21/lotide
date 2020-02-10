c406d8adaa2978a87a49841e1e8ce66d2eed0663
Alex <alextheprogrammer21@gmail.com>
2020-02-10 21:38:57 +0000

    Refactored with assert Equal import/export

--- assertEqual.js
+++ assertEqual.js
@@ -1,14 +1,12 @@
 // FUNCTIONS
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    console.log(`🛑Assertion passed: ${actual} === ${expected}`);
+    console.log(`✅Assertion passed: ${actual} === ${expected}`);
   } else {
-    console.log(`✅Assertion Failed: ${actual} !== ${expected}`);
+    console.log(`🛑Assertion Failed: ${actual} !== ${expected}`);
   }
 };
 
-// TEST CODE
-assertEqual("Lighthouse labs", "Bootcamp");
-assertEqual(1,1);
-assertEqual("Bootcamp", "Bootcamp");
-assertEqual(1,13);
+module.exports = assertEqual;
+
+
--- head.js
+++ head.js
@@ -1,16 +1,7 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🛑Assertion passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`✅Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual');
 
 const head = function(item) {
 return item[0]; 
 };
 
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([5]), 5);
-assertEqual(head([]), 5);
+module.exports = head;
