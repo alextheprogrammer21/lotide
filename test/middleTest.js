0c3da0f25c060048ab3eee9391076828fc38b8ce
Alex <alextheprogrammer21@gmail.com>
2020-02-11 00:08:49 +0000

    commit

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,6 +1,15 @@
-const eqArrays2 = require('../eqArrays');
 const middle = require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 
-assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);// passed
-assertArraysEqual(middle([1,2,5,6]), [1,6]);//failed
+
+// assertArraysEqual(middle([1,2,3,4,5,6]), [3,4]);// passed
+// assertArraysEqual(middle([1,2,5,6]), [1,6]);//failed
+
+
+describe("#Tail", () => {
+  it("Should return [3,4] if you input [1,2,3,4,5,6]", () => {
+    assert.deepEqual(middle([1,2,3,4,5,6]), [3,4]);// passed
+  });
+
+
+});
--- test/tailTest.js
+++ test/tailTest.js
@@ -1,3 +1,15 @@
 const tail = require('../tail');
-const assertEqual = require('../assertEqual');
-assertEqual(tail([0,3]),3);
+const assert = require('chai').assert;
+
+// const assertEqual = require('../assertEqual');
+// assertEqual(tail([0,3]),3);
+
+
+describe("#Tail", () => {
+  it("Should return [3] if you input [0,3]", () => {
+    assert.deepEqual(tail([0, 3]), [3]);
+  });
+
+
+
+});
